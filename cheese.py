16a50a0626ce536db0f58e6c87cdb17449219050
Martin Muzik <41449205+glb32@users.noreply.github.com>
2019-05-20 20:52:32 +0200

    cleanup and sad update
    As of 5.20.2019 this project is discontinued; the new discord API has changed too much and I really can't bother reading the API doc again.

--- cheese.py
+++ cheese.py
@@ -7,22 +7,9 @@ import json
 import time
 
 token = ''
-facts = ["There is no exact information regarding the origin of cheese, but archaeological studies have shown the origin of cheese dates as far back as 6000 BC. Studies also show that during that era, cheese was made from cow’s milk and goats in Mesopotamia.",
-    "There are more than 2000 varieties of cheese available worldwide. Mozzarella is the favorite cheese around the globe, and the most consumed. It is used in pizza and many other recipes.",
-    "People of Greece are the largest consumers of cheese worldwide. An average person from Greece consumes around 27.3 kg of cheese every year, about ¾ of which is feta cheese.",
-    "Pizza Hut is the largest cheese-using fast food giant; it uses approximately 300 million pounds of cheese annually, mostly on pizza.",
-    "Cheese production around the globe is more than the combined worldwide production of coffee, tobacco, tea, and cocoa beans.",
-    "The first cheese factory was established in Switzerland in 1815; however, successful mass production began in 1851 in the United States.",
-    "Contrary to popular belief, cheese, eaten in moderate quantities, is an excellent source of protein, calcium, and phosphorus. Its saturated fat content is responsible for its bad reputation. Even so, the benefits of eating cheese outweigh any negative effects.",
-    "In the United States, the month of June is National Dairy Month and the last week of June is National Cheese Week.",
-    "Cheese can be produced using a variety of milk including cow, buffalo, goat, horse, and even camel.",
-    "A whopping 20 million metric tons of cheese is produced worldwide each year and production is increasing with growing demand.",
-    "Approximately 10 pounds of milk is required to make one pound of cheese. If it wasn’t for cheese, a lot of milk would have been wasted.",
-    "Cheese is kept for a period of time before it’s ready to eat. Some varieties of cheese, blue cheese, Gorgonzola, and brie are exposed to mold, which helps them age properly.",
-    "During the Roman Empire, large Roman houses had separate kitchens for manufacturing cheese only, they were called careale.",
-    "Some varieties of cheese like mozzarella, cheddar, Swiss and American, help prevent tooth decay. They promote the flow of saliva, which leads to elimination of sugar and acids from the mouth.",
-    "Another benefit associated with cheese is that it helps protect tooth enamel and has an antibacterial effect. If consumed in moderate quantities it has various health benefits."
-         ]
+fopen = open("facts.txt")
+def facts(index):
+    fopen.readline(index)
 client = discord.Client()
 
 @client.event
